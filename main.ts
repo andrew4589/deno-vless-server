6e62f5ed02581fe450a3b0354ab72a128e8d5c05
Andrew Jam <kj558500c@gmail.com>
2026-03-22 18:11:57 +0630

    Change default userID to a new UUID

--- main.ts
+++ main.ts
@@ -1,4 +1,4 @@
-const userID = Deno.env.get('UUID') || 'd342d11e-d424-4583-b36e-524ab1f0afa4'
+const userID = Deno.env.get('UUID') || '2b4a7170-35ff-4cf2-905e-e773a8448f80'
 const proxyIP = Deno.env.get('PROXYIP') || ''
 
 if (!isValidUUID(userID)) {
